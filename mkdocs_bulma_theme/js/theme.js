adda3db3e990d14e44f23e5bf7ceb9e5156a87b1
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2023-02-06 21:01:16 +0100

    feat: improve the theme switching feature

--- mkdocs_bulma_theme/js/theme.js
+++ mkdocs_bulma_theme/js/theme.js
@@ -17,4 +17,42 @@ document.addEventListener("DOMContentLoaded", () => {
       $target.classList.toggle("is-active");
     });
   });
+
+  // This code is only used to remember theme selection between page loads.
+  // The first one is the default.
+  const available_themes = ["theme-dark", "theme-light"];
+
+  const themeSwitch = document.querySelector(".theme-switcher");
+  let selectedTheme = Number.parseInt(localStorage.getItem("lastTheme"));
+  setTheme(selectedTheme);
+
+  /**
+   * Toggle the theme of the page.
+   */
+  function setTheme(index) {
+    index = index ?? nextTheme();
+    localStorage.setItem("lastTheme", index);
+    document.body.classList.remove(...available_themes);
+    document.body.classList.add(available_themes[index]);
+  }
+
+  /**
+   * Gets the next theme index to select.
+   *
+   * @returns The next theme index to select.
+   */
+  function nextTheme() {
+    if (selectedTheme === null) {
+      selectedTheme = 0;
+    }
+
+    selectedTheme = (selectedTheme + 1) % available_themes.length;
+    return selectedTheme;
+  }
+
+  themeSwitch.addEventListener("click", function (e) {
+    // const active = e.currentTarget.checked.toString();
+    console.debug("Theme switched to %d", selectedTheme + 1);
+    setTheme();
+  });
 });
